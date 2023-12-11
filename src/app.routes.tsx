87974b5312772f14e4c1126e8255327170ca44bf
Ryu <rastsislauyushkevich@gmail.com>
2023-12-11 16:51:02 +0300

    fixed: routing for auth

--- src/app.routes.tsx
+++ src/app.routes.tsx
@@ -1,3 +1,4 @@
+import AuthRoutes from "auth/auth.routes"
 import ProductsRoutes from "products/products.routes"
 import { FC } from "react"
 import { Route, Routes } from "react-router-dom"
@@ -5,11 +6,14 @@ import { Route, Routes } from "react-router-dom"
 
 
 const AppRoutes: FC = () => {
-  return(
-    <Routes>
-      <Route path='/' element={<div>Home</div>} />
-      <Route path='/products' element={<ProductsRoutes />}/>
-    </Routes>
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<div>Home</div>} />
+        <Route path='/products' element={<ProductsRoutes />} />
+      </Routes>
+      <AuthRoutes />
+    </>
   )
 }
 
