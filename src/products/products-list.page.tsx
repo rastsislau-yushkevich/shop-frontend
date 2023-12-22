af353a5518c63bbe1c3eb680aaf42f0a1e29d9f7
Ryu <rastsislauyushkevich@gmail.com>
2023-12-22 18:16:44 +0300

    feature: very simple pagination

--- src/products/products-list.page.tsx
+++ src/products/products-list.page.tsx
@@ -1,34 +1,49 @@
-import { FC } from "react";
-import { Grid } from "@mui/material";
+import { FC, useState } from "react";
+import { Container, Grid, Pagination } from "@mui/material";
 import ProductCatalogueCard from "./components/product-catalogue-card.comp";
 import { useGetProductsQuery } from "./store/productsApi.slice";
 import { ProductDto } from "./types/product.dto";
 
 const ProductsList: FC = () => {
-  const { data, isLoading } = useGetProductsQuery();
+  const [page, setPage] = useState(1)
+  const { data, isLoading } = useGetProductsQuery({ page, perPage: 8 });
 
   if (isLoading) {
     return <div>Loading...</div>
   }
   const products: ProductDto[] | undefined = data;
 
+  const handlePageChange = (event: any, value: number) => {
+    setPage(value)
+  }
+
   return (
-    <Grid
-      container
-      spacing={4}
-      sx={{ padding: '20px' }}
+    <Container
+      sx={{
+        paddingBottom: '20px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
     >
-      {products?.map(({ id, name, price, image, quantityAvailable }) => (
-        <ProductCatalogueCard
-          key={id}
-          id={id}
-          name={name}
-          price={price}
-          image={image}
-          quantityAvailable={quantityAvailable}
-        />
-      ))}
-    </Grid>
+      <Grid
+        container
+        spacing={4}
+        sx={{ padding: '20px' }}
+      >
+        {products?.map(({ id, name, price, image, quantityAvailable }) => (
+          <ProductCatalogueCard
+            key={id}
+            id={id}
+            name={name}
+            price={price}
+            image={image}
+            quantityAvailable={quantityAvailable}
+          />
+        ))}
+      </Grid>
+      <Pagination page={page} onChange={handlePageChange} count={2} />
+    </Container>
   )
 }
 
